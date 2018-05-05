89bf35386d3cdf3552bfba4514a82b290b338e05
Maxwell Koo <mjkoo90@gmail.com>
2018-05-05 10:27:21 -0700

    Allow testing deploys

--- migrations/2_deploy_bounty_registry.js
+++ migrations/2_deploy_bounty_registry.js
@@ -2,6 +2,12 @@ const NectarToken = artifacts.require('NectarToken');
 const BountyRegistry = artifacts.require('BountyRegistry');
 
 module.exports = function(deployer, network, accounts) {
-  const NECTAR_ADDRESS = '0x9e46a38f5daabe8683e10793b06749eef7d733d1';
-  deployer.deploy(BountyRegistry, NECTAR_ADDRESS);
+  if (network === 'mainnet') {
+    const NECTAR_ADDRESS = '0x9e46a38f5daabe8683e10793b06749eef7d733d1';
+    deployer.deploy(BountyRegistry, NECTAR_ADDRESS);
+  } else {
+    deployer.deploy(NectarToken).then(() => {
+      deployer.deploy(BountyRegistry, NectarToken.address);
+    });
+  }
 };
