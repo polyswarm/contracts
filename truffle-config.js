05d5c6fb98aaa8b13205ff9530fbe8bb0c5fabbc
Maxwell Koo <mjkoo90@gmail.com>
2018-05-06 10:20:06 -0700

    Set gas in truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -7,16 +7,19 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*',
+      gas: 4700000,
     },
     rinkeby: {
       host: 'localhost',
       port: 8545,
       network_id: '4',
+      gas: 4700000,
     },
     mainnet: {
       host: 'localhost',
       port: 8545,
       network_id: '1',
+      gas: 4700000,
     },
   },
   solc: {
