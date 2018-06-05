3c83fe797d5c017b0b350391bc91dc31ee629c5f
Colin Seale <ceseale3@gmail.com>
2018-06-05 03:09:54 -0700

    reduce gas to fix geth issues

--- truffle-config.js
+++ truffle-config.js
@@ -4,22 +4,22 @@ require('babel-polyfill');
 module.exports = {
   networks: {
     development: {
-      host: 'localhost',
+      host: '0.0.0.0',
       port: 8545,
       network_id: '*',
-      gas: 9400000,
+      gas: 5700000,
     },
     rinkeby: {
       host: 'localhost',
       port: 8545,
       network_id: '4',
-      gas: 9400000,
+      gas: 5700000,
     },
     mainnet: {
       host: 'localhost',
       port: 8545,
       network_id: '1',
-      gas: 9400000,
+      gas: 5700000,
     },
   },
   solc: {
