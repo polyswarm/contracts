1a977088928842984c9404ab62abb311a1ce6917
Paul Makowski <pm@polyswarm.io>
2018-07-07 05:26:55 -0400

    move scripts over from contractor

--- scripts/create_config.js
+++ scripts/create_config.js
@@ -4,6 +4,8 @@ const args = require('args-parser')(process.argv);
 const NectarToken = artifacts.require('NectarToken');
 const OfferRegistry = artifacts.require("./OfferRegistry.sol");
 const BountyRegistry = artifacts.require('BountyRegistry');
+const ArbiterStaking = artifacts.require('ArbiterStaking');
+
 const fs = require('fs');
 
 module.exports = async callback => {
@@ -19,11 +21,11 @@ module.exports = async callback => {
   }
 
   if (args.home) {
-    await deployTo(args.home, 'homechain');
+    await deployTo(args.home, args.arbiter, 'homechain');
   }
 
   if (args.side) {
-    await deployTo(args.side, 'sidechain');
+    await deployTo(args.side, args.arbiter, 'sidechain');
   }
 
   writeFile(`${__dirname}/../build/polyswarmd.yml`, config.join('\n'), function(err) {
@@ -39,7 +41,7 @@ module.exports = async callback => {
 
   callback();
 
-  async function deployTo(uri, name) {
+  async function deployTo(uri, arbiter, name) {
     NectarToken.setProvider(new web3.providers.HttpProvider(uri));
     OfferRegistry.setProvider(new web3.providers.HttpProvider(uri));
     BountyRegistry.setProvider(new web3.providers.HttpProvider(uri));
@@ -58,6 +60,24 @@ module.exports = async callback => {
     config.push(`  offer_registry_address: "${offerRegistry.address}"`);
     // TODO: get real address
     config.push(`  erc20_relay_address: "${'0x0000000000000000000000000000000000000000'}"`);
-  }
 
+    await web3.eth.accounts.forEach(async account => {
+      console.log('Minting tokens for ', account);
+      await nectarToken.mint(account, web3.toWei(1000000000, 'ether'));
+    });
+
+    await nectarToken.enableTransfers();
+
+    if (arbiter && web3.isAddress(arbiter)) {
+      console.log('Setting arbiter to: ' + arbiter);
+      console.log(await web3.eth.blockNumber);
+      const arbiterStaking = ArbiterStaking.at(await bountyRegistry.staking());
+      await nectarToken.approve(arbiterStaking.address, web3.toWei(10000000, 'ether'), { from: arbiter });
+      console.log('Staking......')
+      await arbiterStaking.deposit(web3.toWei(10000000, 'ether'), { from: arbiter });
+      await bountyRegistry.addArbiter(arbiter, await web3.eth.blockNumber);
+    }
+
+
+  }
 };
