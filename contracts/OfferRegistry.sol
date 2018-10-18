e64f4d15e1f47b3184a469c2bca5121ba3242db3
Maxwell Koo <mjkoo90@gmail.com>
2018-10-18 14:38:19 -0600

    Address weird indent by solium autoformatter

--- contracts/OfferRegistry.sol
+++ contracts/OfferRegistry.sol
@@ -162,8 +162,9 @@ contract OfferRegistry is Pausable {
 
     function toString(address x) internal pure returns (string) {
         bytes memory b = new bytes(20);
-        for (uint i = 0; i < 20; i++)
-        b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+        for (uint i = 0; i < 20; i++) {
+            b[i] = byte(uint8(uint(x) / (2**(8*(19 - i)))));
+        }
         return string(b);
     }
 
