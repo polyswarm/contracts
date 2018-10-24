59f8041b75145e3ab3c606eeac39c3c4b1a5ef86
Colin Seale <ceseale3@gmail.com>
2018-10-24 11:24:17 -0700

    fix ganache logs in docker
    Signed-off-by: Colin Seale <ceseale3@gmail.com>

--- scripts/test_with_ganache.sh
+++ scripts/test_with_ganache.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-node ./node_modules/ganache-cli/build/cli.node.js --quiet --gasLimit 650000000 --gasPrice 0 >/dev/null 2>/dev/null &
+node ./node_modules/ganache-cli/build/cli.node.js --gasLimit 650000000 --gasPrice 0 >/dev/null 2>/dev/null &
 truffle test
